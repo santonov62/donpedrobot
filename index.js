77125b3a41f99e18f5c32d914f1ab53433d65cb4
Sergey_Antonov <flex62ryz@gmail.com>
2021-01-18 17:13:33 +0300

    Code review

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TOKEN;
+if (!token)
+  throw new Error(`TOKEN required!`);
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -32,7 +34,7 @@ bot.onText(/на баночку (.+)/, (msg, match) => {
       ]
     })
   };
-  bot.sendMessage(msg.chat.id, `${resp}`, opts);
+  bot.sendMessage(chatId, `${resp}`, opts);
 
   // send back the matched "whatever" to the chat
   // bot.sendMessage(chatId, resp);
@@ -41,17 +43,15 @@ bot.onText(/на баночку (.+)/, (msg, match) => {
 // Handle callback queries
 bot.on('callback_query', function onCallbackQuery(callbackQuery) {
   const { data, message, from } = callbackQuery;
-  // const msg = callbackQuery.message;
-  // const from = callbackQuery.message;
   const opts = {
     chat_id: message.chat.id,
     reply_to_message_id: message.message_id,
-    // message_id: msg.message_id,
   };
   let text;
+  const name = from.username || `${from.first_name} ${from.last_name}`
 
   if (data === 'ok') {
-    text = `${from.username} ставит баночку что да`;
+    text = `@${name} ставит баночку что да`;
   }
 
   bot.sendMessage(message.chat.id, text, opts);
