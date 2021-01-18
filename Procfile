66a58ec8ef27ba2814c905f81898394cf5735542
Sergey_Antonov <flex62ryz@gmail.com>
2021-01-18 21:36:11 +0300

    project structure

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: npm start
+web: npm run start-backend
+bot: npm run start-bot
---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('I\'m don pedrobot and i\'m happy!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
