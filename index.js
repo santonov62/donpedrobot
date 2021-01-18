dbb340f293342aa4ac850b4ca9b16a34449d4aa7
Sergey_Antonov <flex62ryz@gmail.com>
2021-01-18 17:06:58 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TOKEN;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/на баночку (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  const opts = {
+    // reply_to_message_id: msg.message_id,
+    reply_markup: JSON.stringify({
+      // keyboard: [
+      //   ['Yes, i\'m in ❤']
+      // ]
+      inline_keyboard: [
+        [
+          {
+            text: 'Yes, i\'m in ❤',
+            // we shall check for this value when we listen
+            // for "callback_query"
+            callback_data: 'ok'
+          }
+        ]
+      ]
+    })
+  };
+  bot.sendMessage(msg.chat.id, `${resp}`, opts);
+
+  // send back the matched "whatever" to the chat
+  // bot.sendMessage(chatId, resp);
+});
+
+// Handle callback queries
+bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+  const { data, message, from } = callbackQuery;
+  // const msg = callbackQuery.message;
+  // const from = callbackQuery.message;
+  const opts = {
+    chat_id: message.chat.id,
+    reply_to_message_id: message.message_id,
+    // message_id: msg.message_id,
+  };
+  let text;
+
+  if (data === 'ok') {
+    text = `${from.username} ставит баночку что да`;
+  }
+
+  bot.sendMessage(message.chat.id, text, opts);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+//
+//   // send a message to the chat acknowledging receipt of their message
+//   bot.sendMessage(chatId, 'Received your message');
+// });
