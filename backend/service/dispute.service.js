e937c028f563333f3327565d54842ce7d288d042
Sergey_Antonov <flex62ryz@gmail.com>
2021-01-20 14:51:42 +0300

    logs

--- backend/service/dispute.service.js
+++ backend/service/dispute.service.js
@@ -7,7 +7,9 @@ AND "resolved_at" IS NULL
 AND "chat_id" IS NOT NULL
 AND "message_id" IS NOT NULL`;
 const getExpired = async () => {
+  log('getExpired')
   const result = await db.query(EXPIRED_DISPUTES, [moment()]);
+  log('getExpired result', result.rows);
   return result.rows;
 }
 
@@ -18,12 +20,14 @@ const ADD_DISPUTE = `INSERT INTO disputes (
 ) RETURNING *`;
 
 const add = async ({title, expired_at, chat_id, message_id}) => {
+  log('add', {title, expired_at, chat_id, message_id})
   const result = await db.query(ADD_DISPUTE, [
     title,
     expired_at,
     chat_id,
     message_id
   ]);
+  log('add result', result.rows[0]);
   return result.rows[0];
 };
 
@@ -34,7 +38,9 @@ WHERE
   id = $1
 RETURNING *`;
 const save = async ({id, expired_at}) => {
+  log('save', {id, expired_at});
   const result = await db.query(UPDATE_DISPUTE_EXPIRED, [id, expired_at]);
+  log('save result', result.rows[0]);
   return result.rows[0];
 };
 
@@ -45,7 +51,9 @@ WHERE
   id = $1
 RETURNING *`;
 const resolve = async ({id}) => {
+  log('resolve', {id});
   const result = await db.query(UPDATE_DISPUTE_RESOLVE, [id, moment()]);
+  log('resolve result', result.rows[0]);
   return result.rows[0];
 };
 
