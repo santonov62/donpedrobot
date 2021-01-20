79a2525c8c43ed42745e51fa583502113a7540d1
Sergey_Antonov <flex62ryz@gmail.com>
2021-01-20 20:21:02 +0300

    rejectUnauthorized

--- backend/service/db.service.js
+++ backend/service/db.service.js
@@ -1,6 +1,11 @@
 const { Pool } = require('pg');
 const connectionString = require('../knexfile')[process.env.NODE_ENV].connection;
-const pool = new Pool({ connectionString: connectionString });
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 pool.connect(err => {
   if (err) throw err;
 });
